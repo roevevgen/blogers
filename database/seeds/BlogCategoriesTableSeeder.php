3119a22c529d2efda5554062e43dbd68f993b5bf
roevevgen <roevevgen7@gmail.com>
2020-01-11 01:19:04 +0200

    first commit

---
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [];
+        $cName = 'Без категории';
+        $categories[] = [
+            'title' => $cName,
+            'slug' => Str::slug($cName),
+            'parent_id' => 0,
+        ];
+
+        for ($i = 0; $i <= 10; $i++) {
+            $cName = 'Категория #' . $i;
+            $parentId = ($i > 4) ? rand(1, 4) : 1;
+            $categories[] = [
+                'title' => $cName,
+                'slug' => Str::slug($cName),
+                'parent_id' => $parentId,
+            ];
+        }
+        Db::table('blog_categories')->insert($categories);
+
+    }
+}
